5add34a4856350252b3b16300b9b33c1526fbaec
Zhigen Zhao <zhigen.zhao@gatech.edu>
2025-10-29 17:01:52 -0400

    minor bug fix

--- general_motion_retargeting/utils/xrt.py
+++ general_motion_retargeting/utils/xrt.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-from general_motion_retargeting.utils.rot_utils import quat_mul_np
 
 try:
     import xrobotoolkit_sdk as xrt_sdk
@@ -19,6 +18,37 @@ XRT_JOINT_NAMES = [
     "Left_Shoulder", "Right_Shoulder", "Left_Elbow", "Right_Elbow",
     "Left_Wrist", "Right_Wrist", "Left_Hand", "Right_Hand"
 ]
+
+def quat_mul_np(x, y, scalar_first=True):
+    """
+    Performs quaternion multiplication on arrays of quaternions
+    :param x: tensor of quaternions of shape (..., 4)
+    :param y: tensor of quaternions of shape (..., 4)
+    :param scalar_first: True if quaternions are in [w, x, y, z] format
+    :return: quaternion multiplication result in same format
+    """
+    if scalar_first:
+        pass
+    else: # convert to scalar-first
+        x = x[..., [3, 0, 1, 2]]
+        y = y[..., [3, 0, 1, 2]]
+
+    x0, x1, x2, x3 = x[..., 0:1], x[..., 1:2], x[..., 2:3], x[..., 3:4]
+    y0, y1, y2, y3 = y[..., 0:1], y[..., 1:2], y[..., 2:3], y[..., 3:4]
+
+    res = np.concatenate([
+        x0 * y0 - x1 * y1 - x2 * y2 - x3 * y3,
+        x0 * y1 + x1 * y0 + x2 * y3 - x3 * y2,
+        x0 * y2 - x1 * y3 + x2 * y0 + x3 * y1,
+        x0 * y3 + x1 * y2 - x2 * y1 + x3 * y0
+    ], axis=-1)
+
+    if scalar_first:
+        pass
+    else:
+        res = res[..., [1, 2, 3, 0]]  # back to [w, x, y, z]
+
+    return res
 
 
 def transform_xr_pose_to_world(position, quaternion):
